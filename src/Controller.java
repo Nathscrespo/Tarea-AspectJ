6d16ee141ac16b346678d0e12fca70bb6c5c5786
nahinespinoza <nahiespi@espol.edu.ec>
2023-10-28 18:20:51 -0500

    Controller

---
+++ src/Controller.java
@@ -0,0 +1,38 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class Controller implements ColorChangeObserver {
+
+    @FXML
+    private Button BtAmarillo;
+
+    @FXML
+    private Button BtRosado;
+
+    @FXML
+    private Button BtNegro;
+
+    @FXML
+    private VBox root_;
+
+    @FXML
+    private void botonAmarillo() {
+        notifyColor("YELLOW");
+    }
+
+    @FXML
+    private void botonRosado() {
+        notifyColor("PINK");
+    }
+
+    @FXML
+    private void botonNegro() {
+        notifyColor("BLACK");
+    }
+
+    @Override
+    public void notifyColor(String colorName) {
+        root_.setStyle("-fx-background-color: " + colorName + ";");
+    }
+}
